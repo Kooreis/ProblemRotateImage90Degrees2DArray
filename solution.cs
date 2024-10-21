a655a3a0dea573d4b8c8ce5169dd4a872dbf0257
Kooreis <kooreis.official@gmail.com>
2024-10-21 00:10:58 -0500

    This update defines the function rotateImage and initializes the variables n, x, and y. n is the length of the image array, x is half of n, and y is n minus 1.

--- solution.cs
+++ solution.cs
@@ -1,29 +1,5 @@
-Here is a JavaScript solution for rotating a 2D array (image) by 90 degrees:
-
-```javascript
 function rotateImage(image) {
     const n = image.length;
     const x = Math.floor(n/ 2);
     const y = n - 1;
-    for (let i = 0; i < x; i++) {
-        for (let j = i; j < y - i; j++) {
-            let k = image[i][j];
-            image[i][j] = image[y - j][i];
-            image[y - j][i] = image[y - i][y - j];
-            image[y - i][y - j] = image[j][y - i]
-            image[j][y - i] = k
-        }
-    }
-    return image;
 }
-
-const image = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-];
-
-console.log(rotateImage(image));
-```
-
-This script defines a function `rotateImage` that takes a 2D array `image` as input and rotates it 90 degrees. The rotated image is then returned. The script then defines a 2D array `image` and calls `rotateImage` with `image` as argument, printing the result to the console.
